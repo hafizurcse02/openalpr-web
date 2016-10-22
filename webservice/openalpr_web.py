bf776b83290fdec2d702e1c7cc16f7582777d931
Minh Tân <nguyenminhtan0612@gmail.com>
2016-10-22 18:44:23 +0700

    F

--- webservice/openalpr_web.py
+++ webservice/openalpr_web.py
@@ -10,6 +10,22 @@ alpr.set_top_n(20)
 
 
 class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        html = """
+                <html>
+                <title>Openalpr Webservice</title>
+                <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> 
+                <body>
+                    <h2>Tesseract Web Service</h2>
+                    <form name="mainForm" id="mainForm" action="" method="POST" enctype="multipart/form-data">
+                        Target image url: <input type="text" id="image" name="image" size="80" />
+                        <input id="submitBtn" type="submit" value="Submit" />
+                    </form>
+                    <div id="result"></div>
+                </body>
+                </html>
+               """
+        self.write(html)
     def post(self):
 
         if 'image' not in self.request.files:
